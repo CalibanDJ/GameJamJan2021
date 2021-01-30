6e418d376c324d0479a9b00de3459c67ea945734
Julian DUPUIS <julian.dupuis@bordeaux-inp.fr>
2021-01-30 12:18:07 +0100

    Conveyorbelt Animation

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -5,6 +5,7 @@ using ItemGen;
 
 public class ItemGenerator : MonoBehaviour
 {
+
     private GrowingRNG itemShapeGen;
     private GrowingRNG itemColorGen;
 
@@ -20,20 +21,6 @@ public class ItemGenerator : MonoBehaviour
 
     public Item itemPrefab;
 
-    // DEFAULT : startPool is half of pool, Step is 1/10
-    public ItemGenerator(GameData gd) {
-        data = gd;
-        itemShapeGen = new GrowingRNG(gd.shapes.Length, gd.shapes.Length / 2, gd.shapes.Length /10);
-        itemColorGen = new GrowingRNG(gd.colors.Length, gd.colors.Length / 2, gd.colors.Length /10);
-
-        // DEFAULT Parameter :
-        minSecond = 1;
-        maxSecondDelay = 10;
-        itemSpawnRateGen = new GrowingRNG(maxSecondDelay, 4, 1); // First parameter + minSecond is the biggest interval between two spawns
-                                                                 // First - (Second parameter - 1) is the initial smallest interval between two spawns
-                                                                 // Third parameter is the step. At each level, the smallest interval is lowered by this parameter.
-        generateNewTimer();
-    }
 
     // Set a Timer between minSecond and minSecond + maxSecondDelay
     private void generateNewTimer() {
@@ -64,9 +51,22 @@ public class ItemGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        data = GameData.Instance;
+        itemShapeGen = new GrowingRNG(data.shapes.Length, data.shapes.Length / 2, data.shapes.Length / 10);
+        itemColorGen = new GrowingRNG(data.colors.Length, data.colors.Length / 2, data.colors.Length / 10);
+
+        // DEFAULT Parameter :
+        minSecond = 1;
+        maxSecondDelay = 10;
+        itemSpawnRateGen = new GrowingRNG(maxSecondDelay, 4, 1); // First parameter + minSecond is the biggest interval between two spawns
+                                                                 // First - (Second parameter - 1) is the initial smallest interval between two spawns
+                                                                 // Third parameter is the step. At each level, the smallest interval is lowered by this parameter.
+        generateNewTimer();
+
         // Au cas ou jsp trop si le constructeur sert
         itemShapeGen = new GrowingRNG(data.shapes.Length, data.shapes.Length / 2, data.shapes.Length /10);
         itemColorGen = new GrowingRNG(data.colors.Length, data.colors.Length / 2, data.colors.Length /10);
+        setSpawnPosition(transform.position);
     }
 
     // Update is called once per frame
