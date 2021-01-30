d9c0d24299f2ac5717c31d1b0bfe5a105439d277
Alexandre <gissaud.alexandre@gmail.com>
2021-01-30 22:08:56 +0100

    Janitor + Floor

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
-    private int nbItems;
+    public int nbItems;
 
     private float initialTimerValue = 5;
     private float timerValue;
@@ -51,11 +51,11 @@ public class Floor : MonoBehaviour
         moveClock();
     }
 
-    void OnTriggerEnter(Collider other) {
+    void OnCollisionEnter2D(Collision2D other) {
         nbItems++;
     }
 
-    void OnTriggerExit(Collider other) {
+    void  OnCollisionExit2D(Collision2D other) {
         nbItems--;
     }
 }
