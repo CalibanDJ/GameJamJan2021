d19a9359652c05482fba5453cdc3ca8eca654b67
Julian DUPUIS <julian.dupuis@bordeaux-inp.fr>
2021-01-31 15:30:59 +0100

    Setting cursor position at middle

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -20,11 +20,11 @@ public class CursorController : MonoBehaviour
     // Update is called once per frame
     public void setClicked()
     {
-        Cursor.SetCursor(clicked, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(clicked, new Vector2(clicked.width/2, clicked.height/2), CursorMode.ForceSoftware);
     }
 
     public void setUnclicked()
     {
-        Cursor.SetCursor(unclicked, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(unclicked, new Vector2(unclicked.width/2, unclicked.height/2), CursorMode.ForceSoftware);
     }
 }
